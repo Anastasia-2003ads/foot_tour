32c1a8f47542e457ca100e863a808fcab93623c9
Anastasia_2003 <anastasia2003.ads@gmail.com>
2025-04-25 17:18:17 +0300

    рефакторинг js кода

--- scripts/scrollEffects.js
+++ scripts/scrollEffects.js
@@ -1,39 +1,58 @@
 let listHotelFeaturesTitles = ['Пляж', 'Отель', 'Питание в отеле', 'Развлечения'];
 
-/* Центрирование элемента внутри прокручиваемого контейнера */
-function centerItemInView(containerSelector, itemSelector) {
-    const container = document.querySelector(containerSelector);
-    const targetItem = document.querySelector(itemSelector);
-    container.scrollLeft = targetItem.offsetLeft
-        - (container.offsetWidth / 2)
-        + (targetItem.offsetWidth / 2);
+/**
+ * Центрирование элемента внутри прокручиваемого контейнера
+ * 
+ * @param {HTMLElement} container - Контейнер для прокрутки
+ * @param {HTMLElement} item - Элемент, который нужно центрировать
+ */
+function centerItemInView(container, item) {
+    if (container && item) {
+        container.scrollLeft = item.offsetLeft
+            - (container.offsetWidth / 2)
+            + (item.offsetWidth / 2);
+    }
 }
 
-/* Активация горизонтальной прокрутки с использованием колесика мыши внутри определенного контейнера */
-function enableHorizontalScroll(containerSelector) {
-    const container = document.querySelector(containerSelector);
-    container.addEventListener('wheel', (e) => {
-        e.preventDefault();
-        container.scrollLeft += e.deltaY;
-    });
+/**
+ * Активация горизонтальной прокрутки с использованием колесика мыши
+ * 
+ * @param {HTMLElement} container - Контейнер, для которого активируется прокрутка
+ */
+function enableHorizontalScroll(container) {
+    if (container) {
+        container.addEventListener('wheel', (e) => {
+            e.preventDefault();
+            container.scrollLeft += e.deltaY;
+        });
+    }
 }
 
-window.onload = function () {
-    // Вызыв для определенных каруселей и элементов после полной загрузки окна
-    centerItemInView('.interesting-for-you__carousel', '.interesting-for-you__list li:nth-child(3)');
-    centerItemInView('.similar-hotels__carousel', '.similar-hotels__list li:nth-child(1)');
+/**
+ * Инициализация каруселей и их элементов, а также установка заголовков 
+ * после полной загрузки окна
+ */
+function initializeFeatures() {
+    centerItemInView(
+        document.querySelector('.interesting-for-you__carousel'),
+        document.querySelector('.interesting-for-you__list li:nth-child(3)')
+    );
+    centerItemInView(
+        document.querySelector('.similar-hotels__carousel'),
+        document.querySelector('.similar-hotels__list li:nth-child(1)')
+    );
 
     const cardsHotelFeatures = document.querySelector('.hotel-features');
-    
     if (cardsHotelFeatures) {
         const queryTitles = document.querySelectorAll('.hotel-features__title');
-
         queryTitles.forEach((node, i) => {
             node.textContent = listHotelFeaturesTitles[i];
         });
     }
-};
 
-// Активация горизонтальной прокрутки для определенных каруселей
-enableHorizontalScroll('.interesting-for-you__carousel');
-enableHorizontalScroll('.similar-hotels__carousel');
+    // Активация горизонтальной прокрутки для каруселей
+    enableHorizontalScroll(document.querySelector('.interesting-for-you__carousel'));
+    enableHorizontalScroll(document.querySelector('.similar-hotels__carousel'));
+}
+
+window.onload = initializeFeatures;
